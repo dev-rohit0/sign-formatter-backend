86a7d8779a577b99fe4e523c061f23be24527988
dev-rohit0 <rohit.codein@gmail.com>
2024-06-30 23:48:28 +0530

    minor fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (!fs.existsSync(outputDir)) {
 }
 
 app.use(cors({
-    origin: 'https://ssc-signature-formatter.vercel.app', // Use your actual domain name (or localhost), using * is not recommended
+    origin: 'https://sscsign.vercel.app', // Use your actual domain name (or localhost), using * is not recommended
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true

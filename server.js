8681b83bb0a0800525e12759b030cce08a1b5250
dev-rohit0 <rohit.codein@gmail.com>
2024-06-30 23:10:20 +0530

    minor fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.use(cors({
-    origin: 'https://ssc-signature-formatter.vercel.app/', // use your actual domain name (or localhost), using * is not recommended
+    origin: 'https://ssc-signature-formatter.vercel.app', // use your actual domain name (or localhost), using * is not recommended
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
